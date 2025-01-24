c7ae0422ca73f23fadc3ddded7e173bac46a812b
kavtonomova <kavtonomova@iqbuzz.ru>
2025-01-24 16:25:19 +0300

    Shifts to all directions, applying moves

--- main.go
+++ main.go
@@ -6,24 +6,32 @@ import (
 )
 
 const (
+	aInANCII = 97
 	//green = "\033[32m"
 	//yellow = "\033[33m"
 	blue = "\033[34m"
 	reset = "\033[0m"
+	notAFile uint64 = 0xfefefefefefefefe
+	notHFile uint64 = 0x7f7f7f7f7f7f7f7f
 )
 
 type GameState struct {
 	White uint64
 	Black uint64
+	IsBlack bool
+	LastMove uint64
+	LastRecolors uint64
 }
 
 func main() {
 	game := GameState{
 		White: 68853694464,
 		Black: 34628173824,
+		IsBlack: true,
 	}
+	directions := []func(uint64)uint64{shiftN, shiftNe, shiftE, shiftSe, shiftS, shiftSw, shiftW, shiftNw}
+	changeBoard(algToBit("f5"), &directions, &game)
 	printBoard(&game)
-	fmt.Println(evaluateGameState(&game))
 }
 
 func printBoard(game *GameState) {
@@ -36,7 +44,7 @@ func printBoard(game *GameState) {
 		if j == 8 {
 			j = 0
 			i ++
-			result += "\n" + blue + strconv.Itoa(i) + reset + " "
+			result += blue + strconv.Itoa(i) + "\n" + strconv.Itoa(i) + reset + " "
 		}
 		if bitMask & game.Black != 0 {
 			result += "x "
@@ -48,6 +56,8 @@ func printBoard(game *GameState) {
 		bitMask <<= 1
 		j++
 	}
+
+	result += blue + "8\n" + "  a b c d e f g h" + reset
 
 	fmt.Println(result)
 }
@@ -68,4 +78,82 @@ func countSetBits(n uint64) int {
 
 func evaluateGameState(game *GameState) int {
 	return countSetBits(game.White) - countSetBits(game.Black)
+}
+
+func algToBit(alg string) uint64 {
+	var result uint64 = 1
+	file := int(alg[0]) - aInANCII
+	rank, _ := strconv.Atoi(string(alg[1]))
+
+	bitsToMove := (rank-1) * 8 + file
+	return result << uint64(bitsToMove)
+}
+
+func changeBoard(move uint64, directions *[]func(uint64)uint64, game *GameState) {
+	var currColor *uint64
+	var oppColor *uint64
+	var recolorBit uint64
+	var allRecolorBits uint64
+	var currRecolorBits uint64
+	
+	if game.IsBlack {
+		currColor = &game.Black
+		oppColor = &game.White
+	} else {
+		currColor = &game.White
+		oppColor = &game.Black
+	}
+
+	game.LastMove = move
+	*currColor |= move
+
+	for _, shift := range *directions {
+		recolorBit = shift(move)
+		currRecolorBits = 0
+
+		for *oppColor & recolorBit != 0 {
+			currRecolorBits |= recolorBit
+			recolorBit = shift(recolorBit)
+		}
+
+		if currRecolorBits != 0 && *currColor & recolorBit != 0 {
+			allRecolorBits |= currRecolorBits
+		}
+	}
+
+	*currColor |= allRecolorBits
+	*oppColor ^= allRecolorBits
+	game.IsBlack = !game.IsBlack
+}
+
+func shiftN(bit uint64) uint64 {
+	return bit << 8
+}
+
+func shiftNe(bit uint64) uint64 {
+	return bit << 9 & notAFile
+}
+
+func shiftE(bit uint64) uint64 {
+	return bit << 1 & notAFile
+}
+
+func shiftSe(bit uint64) uint64 {
+	return bit >> 7 & notAFile
+}
+
+func shiftS(bit uint64) uint64 {
+	return bit >> 8
+}
+
+func shiftSw(bit uint64) uint64 {
+	return bit >> 9 & notHFile
+}
+
+func shiftW(bit uint64) uint64 {
+	return bit >> 1 & notHFile
+}
+
+func shiftNw(bit uint64) uint64 {
+	return bit << 7 & notHFile
 }
